4d28c233686200206b26c7977a35eb84071ede61
eherrador <edgar.herrador@gmail.com>
2020-09-01 20:02:33 +0000

    Adding initial code

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Ahoj Token Issuance
+        </p>
+      </header>
+      <body className="App-body">
+        <p>
+          Hola
+        </p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
